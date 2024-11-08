19260e73eeae05512940a62522a12f4d3d614c9c
oat431 <oat431@gmail.com>
2024-11-09 01:34:34 +0700

    feat: add morgan logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express, { urlencoded } from 'express';
+import express, { json, urlencoded } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import apiDocs from './config/swagger.config.js';
+import morgan from 'morgan';
 
 import healthCheckRouter from './module/HealthCheck/router/health-check.router.js';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
 app.use('/api/v1', healthCheckRouter);
 
